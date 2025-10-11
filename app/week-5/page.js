517716c4cc92d395d7771e8be725b3de22a0a038
Yijie-Alger <Yijie.Wu@edu.sait.ca>
2025-10-11 13:37:57 -0600

    Submit for week 5
    submit files for week 5

---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main className="mx-auto max-w-md p-6">
+      <div>
+        <NewItem />
+      </div>
+    </main>
+  );
+}
